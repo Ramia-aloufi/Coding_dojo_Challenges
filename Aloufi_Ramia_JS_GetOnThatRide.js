e345d27c6eb6458410b2869a88b8010965eaa0be
RAM111111 <r.r.99@windowslive.com>
2021-09-16 20:47:19 +0300

    Aloufi_Ramia_JS_GetOnThatRide.js  uploaded

---
+++ Aloufi_Ramia_JS_GetOnThatRide.js
@@ -0,0 +1,15 @@
+const tall = 52; //the minimum tall neded 52+
+
+
+function check(Chaildtall){
+    //if tall is match
+    if(Chaildtall >tall){
+        console.log("Get on that ride, kiddo!");
+        //tall isn't match
+    }else{
+        console.log("Sorry kiddo. Maybe next year.");
+    }
+};
+//call function and check rider age and tall
+
+check(53);
